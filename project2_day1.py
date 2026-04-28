3e4c0caf608afe2503463ff22a73f9fd06474719
Nayan Neves <neves.devfullstack@gmail.com>
2026-04-28 18:37:05 -0300

    Project 02 - Day 01 - Band Name Generator

---
+++ project2_day1.py
@@ -0,0 +1,7 @@
+# Day 01 — Band Name Generator
+# Gera um nome de banda com base na sua cidade e nome do seu pet
+
+print("Welcome to the Band Name Generator")
+city = input("What's the name of the city you grew up in?\n")
+pet = input("What's your pet's name?\n")
+print(f"Your Band Name could be {city} {pet}")
